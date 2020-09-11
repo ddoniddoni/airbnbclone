5d400f8b4c1e26cc5f7f883a9a1eee505a427c78
ddoniddoni <psdkei@naver.com>
2020-09-11 17:53:31 +0900

    Update Header

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello Let's Start Airbnb-clone</p>
+    <div className="app">
+      <Header />
+      <Home />
     </div>
   );
 }
