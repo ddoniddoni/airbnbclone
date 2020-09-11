10f65cad96eb6abab49e0c45b51bb92d4d9068bb
ddoniddoni <psdkei@naver.com>
2020-09-11 16:46:18 +0900

    Setting react and Airbnb-Clone

---
+++ airbnb-clone/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello Let's Start Airbnb-clone</p>
+    </div>
+  );
+}
+
+export default App;
