ad300b895f4bff168bbe5beb219cef26d2f7a08f
ddoniddoni <psdkei@naver.com>
2020-09-13 21:49:14 +0900

    Update SearchPage and SearchResult

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -4,12 +4,24 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Home from "./Home";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
+
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
